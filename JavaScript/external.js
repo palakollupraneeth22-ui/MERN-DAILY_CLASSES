b28ed7b2f9109ac993568676f12d858fe95f7bf2
Praneeth <palakollupraneeth22@gmail.com>
2026-05-01 09:48:20 +0530

    Day 1 to 9 classes

---
+++ JavaScript/external.js
@@ -0,0 +1,51 @@
+num="java"
+num=50;
+console.log(num);
+//
+
+num=true
+console.log(num);
+//
+
+class sampleexample{
+    display(){
+        console.log("sample example class method called");
+    }
+    text(){
+        console.log("text method called")
+    }
+}
+
+s1=new sampleexample();
+s1.display();
+s1.text();
+
+// function addition(){
+    console.log(10+20);
+//}
+//addition();//
+num=331;
+result = num % 2 == 0 ? "Even" : "Odd";
+console.log(result);
+
+a=10;
+b=20;
+c=null
+d=true;
+e=65865876n
+
+console.log(c==d);
+console.log(c===d);
+
+
+
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
